aac2675f1766aa55f4467d764721185cafe01906
Niko <nikushac@gmail.com>
2019-02-25 11:50:50 +0100

    change post to put request

--- src/server/server.js
+++ src/server/server.js
@@ -27,24 +27,24 @@ app.use(
 
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// app.use(function (req, res, next) {
+//
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 // app.use( express.static( `${__dirname}/../../build` ) );
 //
@@ -120,7 +120,7 @@ app.get("/postValue", (req, res) => {
   );
 });
 
-app.post("/postValue", (req, res) => {
+app.put("/postValue", (req, res) => {
   Node.findByIdAndUpdate(
     req.body.id,
     { $set: { data: {...req.body.data, updated: Date.now() } } },
