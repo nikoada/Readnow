96917248a2f4e13158e59baf9e55db503977ee21
nikushac@gmail.com <nikushac@gmail.com>
2019-02-26 13:41:01 +0000

    perf: adding favicon and other stuff

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00413eb90c2457fefb3e454660a0c0fb.js"
+  "/precache-manifest.9764de06d7489596d0298f7a1e3621c2.js"
 );
 
 workbox.clientsClaim();
